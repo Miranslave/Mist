23e56e015e41a64ad94e5b7b7562fc08f52b9ee7
Zares <76879646+Miranslave@users.noreply.github.com>
2024-01-15 23:01:43 -0600

    Vie de la torche et début de l'ui
    La torche baisse de luminosité en fonction du temps passer dans la brume debut de test de l'ui  avec l'ui toolkit

---
+++ Assets/Movements/Script/PlayerLight.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using UnityEngine.VFX;
+
+public class PlayerLight : MonoBehaviour
+{
+    
+    public VisualEffect vfxRenderer;
+    public float lightlife = 100f;
+    public float overtimedmg;
+    public float timetick;
+    public float radius;
+    private float t;
+    
+
+    private void OnEnable()
+    {
+        t = timetick;
+        radius = vfxRenderer.GetFloat("RadiusLight");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t -=Time.deltaTime;
+        if (t <=0f)
+        {
+            lightlife-= overtimedmg;
+            Debug.Log("Lum tick");
+            t = timetick;
+            Radiuscalculation();
+        }
+        if (lightlife == 0)
+        {
+            Debug.Log("Plus de lumière");
+        }
+        
+    }
+
+    // Recalcule le cercle de protection en fonction du pourcentage de lumière restant
+    void Radiuscalculation()
+    {
+        vfxRenderer.SetFloat("RadiusLight",radius*(lightlife/100));
+    }
+
+    
+    //Player Empower his light consum light faster but radius is stronger
+    void Empower()
+    {
+        vfxRenderer.SetFloat("RadiusLight",8f);
+        overtimedmg = overtimedmg * 2;
+        // attendre 6 secondes  et remettre le cercle à son ancienne valeur plus baiser les dmg par seconde
+    }
+
+
+}
